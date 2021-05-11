0b75df419fd1df78b1efa67ec277aae00176f192
Astrid Aguilar <mixaguilar@yahoo.com>
2021-05-11 22:09:02 +0000

    Added header and new pages for rendering

--- express_server.js
+++ express_server.js
@@ -1,15 +1,26 @@
+// ----------- VARIABLES/FUNCS/ESSENTIALS ---------- //
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// ---------- ROUTES ---------- //
+
+// app.get("/", (req, res) => {
+//   res.send("Hello!");
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -19,6 +30,20 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
+
+// keep this above the route definition below
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", function(req, res) {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[req.params.shortURL];
+
+  res.render("urls_show", {longURL: longURL, shortURL: shortURL});
+});
+
