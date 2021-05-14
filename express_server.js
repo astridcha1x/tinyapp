ebf546790917a5ca86c7791400b9469330e1157d
Astrid Aguilar <mixaguilar@yahoo.com>
2021-05-14 05:44:33 +0000

    created a helper functions module and exported it to be used in express_server.js

--- express_server.js
+++ express_server.js
@@ -38,56 +38,8 @@ const users = {
   }
 };
 
-// GENERATE A RANDOM SHORTURL //
-const generateRandomString = function() {
-
-  const randomChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-  let result = "";
-
-  for (let i = 0; i < 6; i++) {
-    result += randomChars[Math.floor(Math.random() * randomChars.length)];
-  }
-
-  return result;
-
-};
-
-// CHECK FOR EMAIL IN DATABASE //
-const getUserByEmail = function(email, database) {
-
-  for (let user in database) {
-    if (database[user].email === email) {
-      return database[user].id;
-    }
-  }
-
-};
-
-// CHECK FOR PASSWORD IN DATABASE //
-const checkForPassword = function(email, password, users) {
-
-  for (let user in users) {
-    if (users[user].email === email) {
-      return bcrypt.compareSync(password, users[user].password);
-    }
-  }
-
-};
-
-// LINK CHECKER (for user) //
-const checkUserLink = function(object, id) {
-
-  let answer = {};
-
-  for (let item in object) {
-    if (object[item].userID === id) {
-      answer[item] = object[item];
-    }
-  }
-
-  return answer;
-
-};
+// FUNCTIONS FROM HELPERS.JS IMPLEMENTATION //
+const { generateRandomString, getUserByEmail, checkForPassword, checkUserLink } = require ("./helpers");
 
 
 // ---------- ROUTES ---------- //
---
+++ helpers.js
@@ -0,0 +1,58 @@
+const bcrypt = require("bcrypt");
+
+// ----------- FUNCTIONS ----------- //
+
+// GENERATE A RANDOM SHORTURL //
+const generateRandomString = function() {
+
+  const randomChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  let result = "";
+
+  for (let i = 0; i < 6; i++) {
+    result += randomChars[Math.floor(Math.random() * randomChars.length)];
+  }
+
+  return result;
+
+};
+
+// CHECK FOR EMAIL IN DATABASE //
+const getUserByEmail = function(email, database) {
+
+  for (let user in database) {
+    if (database[user].email === email) {
+      return database[user].id;
+    }
+  }
+
+};
+
+// CHECK FOR PASSWORD IN DATABASE //
+const checkForPassword = function(email, password, users) {
+
+  for (let user in users) {
+    if (users[user].email === email) {
+      return bcrypt.compareSync(password, users[user].password);
+    }
+  }
+
+};
+
+// LINK CHECKER (for user) //
+const checkUserLink = function(object, id) {
+
+  let answer = {};
+
+  for (let item in object) {
+    if (object[item].userID === id) {
+      answer[item] = object[item];
+    }
+  }
+
+  return answer;
+
+};
+
+// ----------- EXPORT ----------- //
+
+module.exports = { generateRandomString, getUserByEmail, checkForPassword, checkUserLink };
